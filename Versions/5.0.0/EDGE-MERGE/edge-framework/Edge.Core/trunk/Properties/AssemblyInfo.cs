9ae2229784b73f7589ea62c16b8bdb50449ec1d0
sbarchen <sbarchen@6575f5b9-8578-4614-95a3-f8d796aa64f4>
2015-03-11 18:15:38 +0000

    change adwords processor to ignore dynamic ad and call only ad type

--- Versions/5.0.0/EDGE-MERGE/edge-framework/Edge.Core/trunk/Properties/AssemblyInfo.cs
+++ Versions/5.0.0/EDGE-MERGE/edge-framework/Edge.Core/trunk/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.9.1.1081")]
-[assembly: AssemblyFileVersion("2.9.1.1081")]
+[assembly: AssemblyVersion("2.9.1.10")]
+[assembly: AssemblyFileVersion("2.9.1.10")]
 
